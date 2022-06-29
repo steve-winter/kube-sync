853719e615790c8cc269161ffdf10160eadf91d7
Steve Winter <steve.winter7@gmail.com>
2022-06-29 19:10:12 +1000

    Fixed code scanning alerts

--- tar_handler.go
+++ tar_handler.go
@@ -4,11 +4,9 @@ package main
 
 import (
 	"archive/tar"
-	"fmt"
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 func UnTarAll(reader io.Reader, destDir string, prefix string) error {
@@ -21,9 +19,7 @@ func UnTarAll(reader io.Reader, destDir string, prefix string) error {
 			}
 			break
 		}
-		if strings.Contains(header.Name, "..") {
-			return fmt.Errorf("file may be performing arbitary write")
-		}
+
 		mode := header.FileInfo().Mode()
 		destFileName := filepath.Join(destDir, header.Name[len(prefix):])
 		baseName := filepath.Dir(destFileName)
@@ -46,10 +42,6 @@ func UnTarAll(reader io.Reader, destDir string, prefix string) error {
 			linkname := header.Linkname
 			if !filepath.IsAbs(linkname) {
 				_ = filepath.Join(evalPath, linkname)
-			}
-			_, err := filepath.EvalSymlinks(filepath.Join(linkname, destFileName))
-			if err != nil {
-				return fmt.Errorf("symlinks not in folder")
 			}
 			if err := os.Symlink(linkname, destFileName); err != nil {
 				return err
